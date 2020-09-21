bfd539e77d473f1ccf305f64086e90a9b6b244dd
pula39 <programmed_life@naver.com>
2020-09-21 17:13:48 +0900

    Test 작성

---
+++ WVVServer/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/post_letter', methods=['POST'])
+def tegami():
+    print("letter has come!")
+    return True
