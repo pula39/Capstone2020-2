c446f3d458eff69cc00639144e2f54f75801ba93
pula39 <programmed_life@naver.com>
2020-09-21 22:39:52 +0900

    port 번호를 지정

--- WVVServer/app.py
+++ WVVServer/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,4 +17,6 @@ def tegami():
     print("letter has come!")
     return True
 
-app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
